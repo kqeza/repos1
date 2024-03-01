8770405b495398ab5ecfc9e4224ee8e9561c8573
Kqeza <creepon.752@gmail.com>
2024-03-01 19:02:17 +0300

    1 commit 1 php fiel

--- task2.php
+++ task2.php
@@ -1,1 +1,15 @@
 <?php
+$first_color = readline("Введите первый цвет: ");
+$second_color = readline("Введите второй цвет ");
+
+if ($first_color === 'красный' and $second_color === 'синий') {
+    echo 'фиолетовый';
+} elseif ($first_color === 'красный' and $second_color === 'желтый') {
+    echo 'оранжевый';
+} elseif ($first_color === 'синий' and $second_color === 'желтый') {
+    echo 'зеленый';
+} elseif ($first_color === $second_color) {
+    echo $first_color;
+} else {
+    echo 'Неверный цвет';
+}
--- task3.php
+++ task3.php
@@ -1,1 +1,19 @@
 <?php
+
+function Intersection(int $a1, int $b1, int $a2, int $b2): string
+{
+    $first = max($a1, $a2);
+    $second = min($b1, $b2);
+
+    if ($first > $second) {
+        return "Пересечение пустое";
+    } else if ($first == $second) {
+        return "Пересечение в точке " . $first;
+    } else {
+        return "Пересечение отрезок [" . $first . "; " . $second . "]";
+    }
+}
+
+echo Intersection(2, 6, 4, 8) . PHP_EOL;
+echo Intersection(1, 2, 3, 4) . PHP_EOL;
+echo Intersection(1, 2, 2, 3) . PHP_EOL;
