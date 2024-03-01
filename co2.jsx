da2c65f95727f4d52520132a3d4f3a5845b4a3cf
Kqeza <creepon.752@gmail.com>
2024-03-01 19:01:28 +0300

    1 commit 1 php fiel

--- co2.jsx
+++ co2.jsx
@@ -1,7 +1,7 @@
-function minus (){
-    let g = 8;
-    let p = 4;
-    let r;
-    r = g+p+p*2;
-    console.log(r);
+function plus (){
+    let h = 8;
+    let f = 4;
+    let d;
+    d = f+h;
+    console.log(d);
 }
